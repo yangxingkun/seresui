36d30abb01d7460b5b9bd6eb2a0f565293311bd2
yangxingkun <1612166244@qq.com>
2024-04-19 17:00:20 +0800

    feat:12

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -2,7 +2,7 @@ import run from "../utils/run";
 import { pkgPath } from "../utils/paths";
 import { series } from "gulp";
 export const publishComponent = async () => {
- await  run("release-it", `${pkgPath}/utils`);
- await  run("release-it", `${pkgPath}/seres_ui`);
+//  await  run("release-it", `${pkgPath}/utils`);
+   run("release-it", `${pkgPath}/seres_ui`);
 };
 export default series(async () => publishComponent()) as ()=>void;
